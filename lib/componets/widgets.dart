9bd3f07facf20b34ad65c8e4812d4c2d47b3079c
kiingxo <israelsoyombo@gmail.com>
2024-01-11 07:33:53 +0100

    Update widgets.dart

--- lib/componets/widgets.dart
+++ lib/componets/widgets.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:interview/add_employee.dart';
-import 'package:interview/componets/searchbar.dart';
-import 'package:interview/constants/appconst.dart';
-import 'package:interview/screens/dashboard_page.dart';
 
+import 'package:interview/constants/appconst.dart';
+import 'package:interview/models/employee.dart';
+
+List<Employee> employees = [];
 Widget buildEmployeeDataTable() {
   return Column(
     mainAxisAlignment: MainAxisAlignment.start,
@@ -119,8 +120,55 @@ AppBar buildAppBar() {
         ),
       ),
     ],
-    title: const SearchBar1(
+    title: SearchBar1(
       hinttext: 'Search for groups, employees;settings;etc',
     ),
   );
+}
+
+class SearchBar1 extends StatelessWidget {
+  final String hinttext;
+  final void Function(String)? done;
+  final void Function()? onediting;
+  final TextEditingController? controller;
+  final void Function(String)? onchanged;
+
+  const SearchBar1({
+    Key? key,
+    required this.hinttext,
+    this.done,
+    this.onchanged,
+    this.controller,
+    this.onediting,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(25, 0, 25, 0),
+      child: SizedBox(
+        width: MediaQuery.of(context).size.width * 0.5,
+        child: TextField(
+          onEditingComplete: onediting,
+          onSubmitted: done,
+          onChanged: onchanged,
+          controller: controller,
+          decoration: InputDecoration(
+            filled: true,
+            fillColor: Color.fromARGB(171, 158, 158, 158),
+            hintText: hinttext,
+            hintStyle: const TextStyle(color: Colors.black),
+            suffixIcon: const Icon(Icons.search),
+            border: const OutlineInputBorder(
+              borderRadius: BorderRadius.horizontal(
+                left: Radius.circular(20),
+                right: Radius.circular(20),
+              ),
+              borderSide: BorderSide(color: Colors.black),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
 }
